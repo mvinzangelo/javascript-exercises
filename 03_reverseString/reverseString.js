687a357cad8a737f5663d9e669bc8bc0a84d2300
Vinz Angelo Madrigal <mvinzangelo@gmail.com>
2022-07-06 13:40:49 -0700

    Finish 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (toReverse) {
+    let reversedString = "";
+    for (let i = toReverse.length - 1; i >= 0; i--) {
+        reversedString += toReverse.at(i);
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
