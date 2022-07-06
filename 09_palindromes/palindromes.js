54c53e4414f610ca7d2d5b2fb8db86b58039166e
Vinz Angelo Madrigal <mvinzangelo@gmail.com>
2022-07-06 15:27:12 -0700

    Finish 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    // remove spaces
+    string = string.replace(/\s+/g, '');
+    // remove punctuation
+    string = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+    string = string.toLowerCase();
+    let reverseString = string.split("");
+    reverseString = reverseString.reverse();
+    reverseString = reverseString.join("");
+    return reverseString == string;
 };
 
 // Do not edit below this line
