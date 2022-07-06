6c7a2fed901bf536d5319e329cb0f68b95b1cb7d
Vinz Angelo Madrigal <mvinzangelo@gmail.com>
2022-07-06 14:34:07 -0700

    Finish 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function (x, y) {
+    if (x < 0 || y < 0 || !Number.isFinite(x) || !Number.isFinite(y)) {
+        return "ERROR";
+    }
+    let upperLimit = 0;
+    let lowerLimit = 0;
+    if (x > y) {
+        upperLimit = x;
+        lowerLimit = y;
+    }
+    else {
+        upperLimit = y;
+        lowerLimit = x;
+    }
+    let sum = 0;
+    for (let i = 0; i <= upperLimit - lowerLimit; i++) {
+        sum += lowerLimit + i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
