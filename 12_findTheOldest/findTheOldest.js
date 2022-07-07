b6cc6c4090818dc4b72ecf6ba9d181366f308044
Vinz Angelo Madrigal <mvinzangelo@gmail.com>
2022-07-07 00:06:15 -0700

    Finish 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,30 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+    return arr.reduce(
+        (previousValue, currentValue) => {
+            let previousAge = 0;
+            let currentAge = 0;
+            if (!previousValue.hasOwnProperty("yearOfDeath")) {
+                previousAge = Number(2022) - previousValue.yearOfBirth;
+            }
+            else {
+                previousAge = previousValue.yearOfDeath - previousValue.yearOfBirth;
+            }
+            if (!currentValue.hasOwnProperty("yearOfDeath")) {
+                currentAge = Number(2022) - currentValue.yearOfBirth;
+            }
+            else {
+                currentAge = currentValue.yearOfDeath - currentValue.yearOfBirth;
+            }
+            if (previousAge > currentAge) {
+                return previousValue;
+            }
+            else {
+                return currentValue;
+            }
 
+        }
+
+    );
 };
 
 // Do not edit below this line
